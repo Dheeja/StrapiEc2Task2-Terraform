1d295190a9adde470b5430d4d50a6f207f0d6ed6
Dheeja <jagadeshjaga385@gmail.com>
2025-04-22 23:52:58 +0530

    Rex

--- main.tf
+++ main.tf
@@ -49,37 +49,7 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public.id
 }
 
-# Security Group
-resource "aws_security_group" "allow_ssh_web" {
-  name        = "allow_ssh_web"
-  description = "Allow SSH and Strapi"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow-ssh-and-web"
-  }
-}
+# 
 
 # EC2 Instance
 resource "aws_instance" "strapi_ec2" {
---
+++ security.tf
@@ -0,0 +1,32 @@
+
+#Security Group
+resource "aws_security_group" "allow_ssh_web" {
+  name        = "allow_ssh_web"
+  description = "Allow SSH and Strapi"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh-and-web"
+  }
+}
